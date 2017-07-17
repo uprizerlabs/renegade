5fafc1212a9498af0f4e4ff83270cb3aebc6c46a
Ian Clarke <github@ian.33mail.com>
2017-07-17 18:14:09 -0500

    add the CV and mnist stuff back

---
+++ src/main/kotlin/dnn/crossValidation/CrossValidator.kt
@@ -0,0 +1,49 @@
+package dnn.crossValidation
+
+import mu.KotlinLogging
+import java.util.*
+
+/**
+ * Created by ian on 7/16/17.
+ */
+
+private val logger = KotlinLogging.logger {}
+
+typealias InputOutputPair<T, U> = Pair<T, U>
+
+class CrossValidator<out InputType : Any, out OutputType : Any, in PredictionType : Any>(
+        private val splitStrategy: SplitStrategy<InputType, OutputType>,
+        private val scoringFunction: (PredictionType, OutputType) -> Double,
+        private val data: Iterable<InputOutputPair<InputType, OutputType>>
+) {
+    fun test(predictiveModelBuilder: (List<InputOutputPair<InputType, OutputType>>) -> ((InputType) -> PredictionType)): Double {
+        val loss = DoubleSummaryStatistics()
+        splitStrategy.split(data).forEach { (training, testing) ->
+            logger.info("Building model")
+            val model = predictiveModelBuilder.invoke(training)
+            logger.info("Testing model")
+            for ((input, output) in testing) {
+                loss.accept(scoringFunction.invoke(model.invoke(input), output))
+            }
+        }
+        loss.apply {
+            logger.info("Tested on $count test datums, loss is $average")
+        }
+        return loss.average
+    }
+}
+
+data class TrainTest<InputType : Any, OutputType : Any>(
+        val training: ArrayList<InputOutputPair<InputType, OutputType>>,
+        val testing: ArrayList<InputOutputPair<InputType, OutputType>>
+)
+
+class CorrectClassificationProportion<in OutputType : Any> : (OutputType, OutputType) -> Double {
+    override fun invoke(output: OutputType, classification: OutputType) = if (output == classification) 1.0 else 0.0
+
+}
+
+interface SplitStrategy<InputType : Any, OutputType : Any> {
+    fun split(data: Iterable<InputOutputPair<InputType, OutputType>>): Sequence<TrainTest<InputType, OutputType>>
+}
+
---
+++ src/main/kotlin/dnn/crossValidation/FoldSplitStrategy.kt
@@ -0,0 +1,33 @@
+package dnn.crossValidation
+
+import mu.KotlinLogging
+import java.util.*
+import kotlin.coroutines.experimental.buildSequence
+
+private val logger = KotlinLogging.logger {}
+
+class FoldSplitStrategy<InputType : Any, OutputType : Any>(private val folds: Int, private val limit: Int? = null) : SplitStrategy<InputType, OutputType> {
+    private val largePrimeNumber = 10007
+
+    override fun split(data: Iterable<InputOutputPair<InputType, OutputType>>): Sequence<TrainTest<InputType, OutputType>> {
+        val foldsWithLimit = if (limit != null) Math.min(folds, limit) else folds
+        return buildSequence {
+            for (fold in 0..foldsWithLimit) {
+                logger.info("Splitting on fold $fold of $foldsWithLimit")
+                val training = ArrayList<InputOutputPair<InputType, OutputType>>()
+                val testing = ArrayList<InputOutputPair<InputType, OutputType>>()
+                for (datum in data) {
+                    val isTesting = hashToFold(datum) == fold
+                    if (isTesting) {
+                        testing += datum
+                    } else {
+                        training += datum
+                    }
+                }
+                yield(TrainTest(training, testing))
+            }
+        }
+    }
+
+    private fun hashToFold(datum: InputOutputPair<InputType, OutputType>) = Math.abs(datum.hashCode()).rem(largePrimeNumber).rem(folds)
+}
